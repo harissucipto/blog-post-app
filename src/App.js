c4927e926603d0783b04d5c7b27e33ece814f3d5
Haris Sucipto <harisssucipto@gmail.com>
2019-11-20 11:47:50 +0700

    changing setUsername  to global

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import PostList from './post/PostList';
 import CreatePost from './post/CreatePost';
 import UserBar from './user/UserBar';
 
-const user = 'Daniel Bugl';
 const posts = [
   {
     title: 'React Hooks',
@@ -19,11 +18,13 @@ const posts = [
 ];
 
 export default function App() {
+  const [user, setUser] = useState('');
+
   return (
     <div style={{ padding: 8 }}>
-      <UserBar />
+      <UserBar user={user} setUser={setUser} />
       <br />
-      <CreatePost user={user} />
+      {user && <CreatePost user={user} />}
       <br />
       <hr />
       <PostList posts={posts} />
--- src/user/UserBar.js
+++ src/user/UserBar.js
@@ -1,12 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import Login from './Login';
 import Logout from './Logout';
 import Register from './Register';
 
-export default function UserBar() {
-  const [user, setUser] = useState('');
-
+export default function UserBar({ user, setUser }) {
   if (user) {
     return <Logout user={user} setUser={setUser} />;
   } else {
