5958fb73b77cc500515793e39f58577b73d24c0e
Haris Sucipto <harisssucipto@gmail.com>
2019-11-20 11:07:11 +0700

    add user statis component

---
+++ src/user/UserBar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import Login from './Login';
+import Logout from './Logout';
+import Register from './Register';
+
+export default function UserBar() {
+  const user = 'Haris Sucipto';
+
+  if (user) {
+    return <Logout user={user} />;
+  } else {
+    return (
+      <React.Fragment>
+        <Login />
+        <Register />
+      </React.Fragment>
+    );
+  }
+}
