9db048efd9efd7979933a531eb29580c0635858f
Haris Sucipto <harisssucipto@gmail.com>
2019-11-21 20:04:13 +0700

    optimize for not rerender

--- src/post/Post.js
+++ src/post/Post.js
@@ -1,7 +1,8 @@
 import React, { useContext } from 'react';
 import { ThemeContext } from '../contexts';
 
-export default function Post({ title, content, author }) {
+function Post({ title, content, author }) {
+  console.log('rendering Post');
   const { secondaryColor } = useContext(ThemeContext);
 
   return (
@@ -15,3 +16,11 @@ export default function Post({ title, content, author }) {
     </div>
   );
 }
+
+export default React.memo(
+  Post,
+  (prev, next) =>
+    prev.title === next.title &&
+    prev.content === next.content &&
+    prev.author === next.author
+);
