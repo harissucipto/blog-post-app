85f34f939c72103c3e6cccc912e678079f6e477e
Haris Sucipto <harisssucipto@gmail.com>
2019-11-24 14:34:55 +0700

    sprate component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React, { useReducer, useEffect, useState } from 'react';
-import { useResource } from 'react-request-hook';
 
 import appReducer from './reducers';
 import { ThemeContext, StateContext } from './contexts';
-
-import PostList from './post/PostList';
-import CreatePost from './post/CreatePost';
-import UserBar from './user/UserBar';
-import Header from './Header';
-import ChangeTheme from './ChangeTheme';
+import HeaderBar from './pages/HeaderBar';
+import HomePage from './pages/HomePage';
 
 export default function App() {
   const [theme, setTheme] = useState({
@@ -22,24 +17,7 @@ export default function App() {
     error: ''
   });
 
-  const { user, error } = state;
-
-  const [posts, getPosts] = useResource(() => ({
-    url: '/posts',
-    method: 'get'
-  }));
-
-  useEffect(getPosts, []);
-
-  useEffect(() => {
-    if (posts && posts.error) {
-      dispatch({ type: 'POSTS_ERROR' });
-    }
-
-    if (posts && posts.data) {
-      dispatch({ type: 'FETCH_POSTS', posts: posts.data.reverse() });
-    }
-  }, [posts]);
+  const { user } = state;
 
   useEffect(() => {
     if (user) {
@@ -53,19 +31,9 @@ export default function App() {
     <StateContext.Provider value={{ state, dispatch }}>
       <ThemeContext.Provider value={theme}>
         <div style={{ padding: 8 }}>
-          <Header text="React Hooks Blog" />
-          <ChangeTheme theme={theme} setTheme={setTheme} />
-          <br />
-          <React.Suspense fallback={'Loading...'}>
-            <UserBar />
-          </React.Suspense>
-
-          <br />
-          {user && <CreatePost />}
-          <br />
+          <HeaderBar setTheme={setTheme} />
           <hr />
-          {error && <b>{error}</b>}
-          <PostList />
+          <HomePage />
         </div>
       </ThemeContext.Provider>
     </StateContext.Provider>
