a59a8afc2998698441e40bcef5894d67e5f624de
Haris Sucipto <harisssucipto@gmail.com>
2019-12-10 09:12:03 +0700

    testing useCounter

---
+++ src/hooks/useCounter.test.js
@@ -0,0 +1,32 @@
+import { renderHook, act } from "@testing-library/react-hooks";
+
+import useCounter from "./useCounter";
+
+test("should use counter", () => {
+  const { result } = renderHook(() => useCounter());
+  expect(result.currnet.count).toBe(0);
+  expect(typeof result.current.increment).toBe("function");
+});
+
+test("should increment counter", () => {
+  const { result } = renderHook(() => useCounter());
+  act(() => result.current.increment());
+  expect(result.current.count).toBe(1);
+});
+
+test("should use initial value", () => {
+  const { result } = renderHook(() => useCounter(123));
+  expect(result.current.count).toBe(123);
+  act(() => result.current.increment());
+  expect(result.current.count).toBe(124);
+});
+
+// testing reset and forcing re-rendering
+test("should reset to inital value", () => {
+  let initial = 0;
+  const { result, rerender } = renderHook(() => useCounter(initial));
+  initial = 123;
+  rerender();
+  act(() => result.current.reset());
+  expect(result.current.count).toBe(123);
+});
